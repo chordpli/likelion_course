238c9f5afc2c68759e68ff875433493723bf595e
Junho Kim <chordplaylist@gmail.com>
2022-10-04 14:44:34 +0900

    221004 화폐계산2 요구사항 발전

--- src/week3/day2/CountMoney2.java
+++ src/week3/day2/CountMoney2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CountMoney2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input = 25000;
+        int input = 25400;
         // 25000원을 받고 화폐별 몇 장으로 변환이 가능하며, 남은 금액은 얼마인가?
 
         int money50000 = 50000;
@@ -18,22 +18,29 @@ public class CountMoney2 {
         int money10 = 10;
 
         // 50000원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("50000권 : %d장, 나머지 : %d 원 \n", input / money50000, input % money50000);
+        System.out.printf("50000권 : %d개, 나머지 : %d 원 \n", input / money50000, input % money50000);
+        input %= money50000;
         // 10000원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("10000권 : %d장, 나머지 : %d 원 \n", input / money10000, input % money10000);
+        System.out.printf("10000권 : %d개, 나머지 : %d 원 \n", input / money10000, input % money10000);
+        input %= money10000;
         // 5000원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("5000권 : %d장, 나머지 : %d 원 \n", input / money5000, input % money5000);
+        System.out.printf("5000권 : %d개, 나머지 : %d 원 \n", input / money5000, input % money5000);
+        input %= money5000;
         // 1000원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("1000권 : %d장, 나머지 : %d 원 \n", input / money1000, input % money1000);
+        System.out.printf("1000권 : %d개, 나머지 : %d 원 \n", input / money1000, input % money1000);
+        input %= money1000;
         // 500원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("500권 : %d장, 나머지 : %d 원 \n", input / money500, input % money500);
+        System.out.printf("500권 : %d개, 나머지 : %d 원 \n", input / money500, input % money500);
+        input %= money500;
         // 100원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("100권 : %d장, 나머지 : %d 원 \n", input / money100, input % money100);
+        System.out.printf("100권 : %d개, 나머지 : %d 원 \n", input / money100, input % money100);
+        input %= money100;
         // 50원으로 변환했을 때, 변환 후 남은 금액
-        System.out.printf("50권 : %d장, 나머지 : %d 원 \n", input / money50, input % money50);
+        System.out.printf("50권 : %d개, 나머지 : %d 원 \n", input / money50, input % money50);
+        input %= money50;
         // 10원으로 변환했을 때, 변환 후 남은 금액
         System.out.printf("10권 : %d장, 나머지 : %d 원 \n", input / money10, input % money10);
-
+        input %= money10;
 
         sc.close();
     }
