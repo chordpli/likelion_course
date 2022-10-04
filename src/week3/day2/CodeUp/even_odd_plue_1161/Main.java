05df80885a72ed1314c919badec71a43f6b5221d
Junho Kim <chordplaylist@gmail.com>
2022-10-04 15:30:01 +0900

    221004 홀수와 짝수 더하기(CodeUp)

--- src/week3/day2/CodeUp/even_odd_plue_1161/Main.java
+++ src/week3/day2/CodeUp/even_odd_plue_1161/Main.java
@@ -3,31 +3,30 @@ package week3.day2.CodeUp.even_odd_plue_1161;
 import java.util.Scanner;
 
 public class Main {
+
+    private String getEvenOdd(int num) {
+        if(num % 2== 0){
+            return "짝수";
+        }else{
+            return "홀수";
+        }
+    }
+
+    public void printEvenOddStatement(int first, int second) {
+        System.out.printf("%s+%s=%s",
+                getEvenOdd(first),
+                getEvenOdd(second),
+                getEvenOdd(first+second));
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try {
-            int num1 = sc.nextInt();
-            int num2 = sc.nextInt();
+        Main main = new Main();
 
-            if (num1 % 2 == 0) {
-                System.out.print("짝수+");
-            } else {
-                System.out.print("홀수+");
-            }
-
-            if (num2 % 2 == 0) {
-                System.out.print("짝수=");
-            } else {
-                System.out.print("홀수=");
-            }
-
-            if ((num1 + num2) % 2 == 0) {
-                System.out.print("짝수");
-            } else {
-                System.out.print("홀수");
-            }
-        } catch (Exception e) {
-            System.out.println("숫자를 입력해주세요");
+        try{
+            main.printEvenOddStatement(sc.nextInt(), sc.nextInt());
+        }catch (Exception e){
+            System.out.println("숫자를 입력해주세요.");
         }
     }
 }
