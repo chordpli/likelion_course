0edd3662bb7bde8aa82e997938fce87ceea1ab97
Junho Kim <chordplaylist@gmail.com>
2022-10-04 13:49:21 +0900

    221004 화폐계산

--- src/week3/day2/CountMoney.java
+++ src/week3/day2/CountMoney.java
@@ -1,6 +1,5 @@
 package week3.day2;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class CountMoney {
@@ -10,18 +9,12 @@ public class CountMoney {
         String[] moneyName = {"50000원", "10000원", "5000원", "1000원", "500원", "100원", "50원", "10원"};
         int[] money = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 
-        int input = 25300;
+        int input = sc.nextInt();
         int count = 0;
         for (int i = 0; i < 8; i++) {
             count = input / money[i];
-            if (count == 0) {
-                if (input == 0) {
-                    break;
-                }
-            } else {
-                System.out.println(moneyName[i] + count + "개");
-                input -= money[i] * count;
-            }
+            System.out.println(moneyName[i] + count + "개");
+            input -= money[i] * count;
         }
     }
 }
