fb50a17ceb11cbd6f0ea13156c1539a09d81bab5
Junho Kim <chordplaylist@gmail.com>
2022-10-04 15:21:37 +0900

    221004 홀수와 짝수 더하기(CodeUp)

--- src/week3/day2/CodeUp/even_odd_plue_1161/Main.java
+++ src/week3/day2/CodeUp/even_odd_plue_1161/Main.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try{
+        try {
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-    
+
             if (num1 % 2 == 0) {
                 System.out.print("짝수+");
             } else {
                 System.out.print("홀수+");
             }
-    
+
             if (num2 % 2 == 0) {
                 System.out.print("짝수=");
             } else {
                 System.out.print("홀수=");
             }
-    
+
             if ((num1 + num2) % 2 == 0) {
                 System.out.print("짝수");
             } else {
                 System.out.print("홀수");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("숫자를 입력해주세요");
         }
     }
