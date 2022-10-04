602448d0a60e6d246e8bc4548d8d1be577f9700d
Junho Kim <chordplaylist@gmail.com>
2022-10-04 13:19:24 +0900

    221004 Array

---
+++ src/week2/day2/ArrayTest.java
@@ -0,0 +1,15 @@
+package week2.day2;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        int[] intArrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] intArrays2 = new int[10];
+
+        for(int i = 0; i <= 9; i++){
+            intArrays2[i] = i+1;
+        }
+
+    }
+}
