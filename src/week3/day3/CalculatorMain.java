4f23c77a8f7c57f92b42a30c487cb59b7a131d5c
Junho Kim <chordplaylist@gmail.com>
2022-10-05 10:14:52 +0900

    221005 Calculator

---
+++ src/week3/day3/CalculatorMain.java
@@ -0,0 +1,8 @@
+package week3.day3;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator userDao = new Calculator();
+        userDao.plus(3,5);
+    }
+}
