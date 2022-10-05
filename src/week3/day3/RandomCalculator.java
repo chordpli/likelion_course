89a6b200fa6609157966fcb702487c348d4094ee
Junho Kim <chordplaylist@gmail.com>
2022-10-05 11:36:36 +0900

    221005 RandomCalculator

--- src/week3/day3/RandomCalculator.java
+++ src/week3/day3/RandomCalculator.java
@@ -1,30 +1,32 @@
 package week3.day3;
 
-import java.util.Random;
-
 public class RandomCalculator {
     private RandomNumberCreator random = new RandomNumberCreator();
+    private int randomNumber;
     //private int random = (int)(Math.random()*9);
 
+    public RandomCalculator() {
+        this.randomNumber = random.randomNumberCreator();
+    }
+
     public void plus(int a) {
-        System.out.println(a + random.randomNumberCreator());
+        System.out.println(a + randomNumber);
     }
 
     public void minus(int a) {
-        System.out.println(a - random.randomNumberCreator());
+        System.out.println(a - randomNumber);
     }
 
     public void divide(int a) {
-        try{
-            float divide = (float) a/ random.randomNumberCreator();
+        try {
+            float divide = (float) a / randomNumber;
             System.out.println(divide);
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("랜덤하게 생성된 숫자가 0입니다.");
         }
-
     }
 
     public void multiple(int a) {
-        System.out.println(a * random.randomNumberCreator());
+        System.out.println(a * randomNumber);
     }
 }
