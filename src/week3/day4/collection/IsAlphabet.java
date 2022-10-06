deaa01d5e1aa16e4335efcf1ef6d8b89d32439ec
Junho Kim <chordplaylist@gmail.com>
2022-10-06 13:28:34 +0900

    221006 isAlphabet static 제거

--- src/week3/day4/collection/IsAlphabet.java
+++ src/week3/day4/collection/IsAlphabet.java
@@ -2,15 +2,17 @@ package week3.day4.collection;
 
 public class IsAlphabet {
     public static void main(String[] args) {
+        IsAlphabet isAlphabet = new IsAlphabet();
+
         String str = "abcdefg1234";
         for(int i = 0; i < str.length(); i++){
-            if(isAlphabet(str.charAt(i))){
+            if(isAlphabet.isAlphabet(str.charAt(i))){
                 System.out.print(str.charAt(i) + " ");
             }
         }
     }
 
-    public static boolean isAlphabet(char alphabet){
+    public boolean isAlphabet(char alphabet){
         boolean result = false;
         for(char c = 'a' ; c <= 'z' ; c++){
             if(alphabet == c){
