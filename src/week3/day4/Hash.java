6a9213e2e00321144ca52b7b5353aa09a34af6b3
Junho Kim <chordplaylist@gmail.com>
2022-10-06 21:18:34 +0900

    221006 Hash

---
+++ src/week3/day4/Hash.java
@@ -0,0 +1,9 @@
+package week3.day4;
+
+import java.util.Hashtable;
+
+public class Hash {
+    public static void main(String[] args) {
+        Hashtable<String, String> hashtable = new Hashtable<>();
+    }
+}
