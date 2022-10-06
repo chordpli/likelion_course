f8a6760dfefa9280130d0f7f43d364c0c1f28922
Junho Kim <chordplaylist@gmail.com>
2022-10-06 15:33:27 +0900

    221006 readAChar, read2Chars

--- src/week3/day4/file/FileExercise.java
+++ src/week3/day4/file/FileExercise.java
@@ -2,17 +2,39 @@ package week3.day4.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileExercise {
-    public static void main(String[] args) throws FileNotFoundException {
-        //FileReader fileReader = new FileReader("./a_file.txt");
+
+    public void printFiles(){
         File dir = new File("."); // 디렉토리로 접근
         File files[] = dir.listFiles(); // 파일들이 담김
 
         for (File f : files) {
             System.out.println(f);
         }
-        //System.out.println(files[0]);
+    }
+
+    public char readAChar(String filename) throws IOException {
+        FileReader fileReader = new FileReader(filename);
+        return (char)fileReader.read();
+    }
+
+    public String read2Chars(String filename) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str += (char)fileReader.read();
+        str += (char)fileReader.read();
+        return str;
+    }
+
+    public static void main(String[] args) throws Exception {
+        FileExercise fileExercise = new FileExercise();
+        char c = fileExercise.readAChar("./a_file.txt");
+        String str = fileExercise.read2Chars("./a_file.txt");
+        System.out.println(c);
+        System.out.println(str);
     }
 /*
     .\.git
