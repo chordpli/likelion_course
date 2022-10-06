aaf71d5d3b59d5c7ab8b18e5aa7f7ff11816c268
Junho Kim <chordplaylist@gmail.com>
2022-10-06 14:28:54 +0900

    221006 readAByte

---
+++ src/week3/day4/file/ReadFile.java
@@ -0,0 +1,30 @@
+package week3.day4.file;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadFile {
+
+    private String filename;
+
+    public ReadFile(String filename) {
+        this.filename = filename;
+    }
+
+    public char readAByte() throws Exception {
+        try (BufferedReader br = Files.newBufferedReader(
+                Paths.get(filename), StandardCharsets.UTF_8)) {
+            return br.readLine().charAt(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        ReadFile readFile = new ReadFile("./a_file.txt");
+        System.out.println(readFile.readAByte());
+    }
+}
