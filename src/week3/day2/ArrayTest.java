0192f12c067ac0d9dc355fe2f8c1d7da139a54ea
Junho Kim <chordplaylist@gmail.com>
2022-10-06 10:33:42 +0900

    221006 List

--- src/week3/day2/ArrayTest.java
+++ src/week3/day2/ArrayTest.java
@@ -1,12 +1,15 @@
 package week3.day2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayTest {
     public static void main(String[] args) {
         System.out.println("Hello");
 
         int[] intArrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] intArrays2 = new int[10];
-
+        List<Integer> intarr = new ArrayList<>();
         for(int i = 0; i <= 9; i++){
             intArrays2[i] = i+1;
         }
