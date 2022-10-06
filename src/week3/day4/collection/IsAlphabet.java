4b9b34a3ea00a1c8390a490b888183d48e0bef49
Junho Kim <chordplaylist@gmail.com>
2022-10-06 15:12:12 +0900

    221006 isAlphabet

--- src/week3/day4/collection/IsAlphabet.java
+++ src/week3/day4/collection/IsAlphabet.java
@@ -1,10 +1,12 @@
 package week3.day4.collection;
 
+import java.util.Locale;
+
 public class IsAlphabet {
     public static void main(String[] args) {
         IsAlphabet isAlphabet = new IsAlphabet();
 
-        String str = "abcdefg1234";
+        String str = "abcdefg1234".toUpperCase();
         for(int i = 0; i < str.length(); i++){
             if(isAlphabet.isAlphabet(str.charAt(i))){
                 System.out.print(str.charAt(i) + " ");
@@ -14,7 +16,7 @@ public class IsAlphabet {
 
     public boolean isAlphabet(char alphabet){
         boolean result = false;
-        for(char c = 'a' ; c <= 'z' ; c++){
+        for(char c = 'A' ; c <= 'Z' ; c++){
             if(alphabet == c){
                 result = true;
             }
