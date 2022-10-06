d38652c52fd2f5cea50d477da9b42779e0786e4f
Junho Kim <chordplaylist@gmail.com>
2022-10-06 15:44:36 +0900

    221006 read method

--- src/week3/day4/file/FileExercise.java
+++ src/week3/day4/file/FileExercise.java
@@ -1,13 +1,15 @@
 package week3.day4.file;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileExercise {
+    private String filename;
 
-    public void printFiles(){
+    public FileExercise(String filename) {
+        this.filename = filename;
+    }
+
+    public void printFiles() {
         File dir = new File("."); // 디렉토리로 접근
         File files[] = dir.listFiles(); // 파일들이 담김
 
@@ -16,25 +18,65 @@ public class FileExercise {
         }
     }
 
-    public char readAChar(String filename) throws IOException {
+    public char readAChar() throws IOException {
         FileReader fileReader = new FileReader(filename);
-        return (char)fileReader.read();
+        return (char) fileReader.read();
     }
 
-    public String read2Chars(String filename) throws IOException{
+    public String read2Chars() throws IOException {
         FileReader fileReader = new FileReader(filename);
         String str = "";
-        str += (char)fileReader.read();
-        str += (char)fileReader.read();
+        str += (char) fileReader.read();
+        str += (char) fileReader.read();
         return str;
     }
 
+    public String readOptionByte(int Option) throws Exception {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024)) {
+            String line = br.readLine();
+            String str = "";
+            for (int i = 0; i < Option; i++) {
+                str += line.charAt(i);
+            }
+            return str;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readOneLine() throws Exception {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024)) {
+            String line = br.readLine();
+            return line;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void readAllLine() throws Exception {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename), 16 * 1024)) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        FileExercise fileExercise = new FileExercise();
-        char c = fileExercise.readAChar("./a_file.txt");
-        String str = fileExercise.read2Chars("./a_file.txt");
-        System.out.println(c);
-        System.out.println(str);
+        FileExercise fileExercise = new FileExercise("./a_file.txt");
+        char char1 = fileExercise.readAChar();
+        String str2 = fileExercise.read2Chars();
+        String str3 = fileExercise.readOptionByte(3);
+
+        String line = fileExercise.readOneLine();
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(line);
+
+        fileExercise.readAllLine();
     }
 /*
     .\.git
