24a09a343ae0264c419e2ac69965583cd9e565e9
Junho Kim <chordplaylist@gmail.com>
2022-10-11 09:24:19 +0900

    221011 프로그래머스 자릿수 더하기

--- src/week4/programmers/Solution.java
+++ src/week4/programmers/Solution.java
@@ -1,12 +1,24 @@
 package week4.programmers;
 
 public class Solution {
-    public int solution(int n){
+    // 문자열 변환 후 자릿수별 숫자 더하기
+    public int solution(int n) {
         String[] number = String.valueOf(n).split("");
         int result = 0;
-        for(int i = 0; i < number.length; i++){
+        for (int i = 0; i < number.length; i++) {
             result += Integer.parseInt(number[i]);
         }
-        return  result;
+        return result;
     }
+    
+    // 나머지 값으로 자릿수별 숫자 더하기
+    public int solution2(int n) {
+        int result = 0;
+        while (n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+        return result;
+    }
+
 }
