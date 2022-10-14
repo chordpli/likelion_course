3c1d569a7ab2294d4075584fdf49cdab38d0eefc
chordpli <chordplaylist@gmail.com>
2022-10-14 10:45:53 +0900

    221013 InsertionSort

--- src/week4/day5/algorithm/insertionsort/InsertionSort99.java
+++ src/week4/day5/algorithm/insertionsort/InsertionSort99.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class InsertionSort99 {
 
     public int[] sort(int[] arr, int i) {
-
-        if(i == arr.length) return  arr;
+        if (i == arr.length) {
+            return arr;
+        }
         for (int j = i; j > 0; j--) {
-            System.out.printf("i:%d j:%d j-1:%d\n", i, j, j-1);
             if (arr[j] < arr[j - 1]) {
                 arr = swap(arr, j);
             }
         }
-
-        return  sort(arr, i+1);
+        return sort(arr, i + 1);
     }
 
     public int[] swap(int[] arr, int j) {
@@ -28,7 +27,7 @@ public class InsertionSort99 {
         int[] arr = {8, 5, 6, 2, 4};
 
         InsertionSort99 is = new InsertionSort99();
-        arr = is.sort(arr,1);
+        arr = is.sort(arr, 1);
 
         System.out.println(Arrays.toString(arr));
     }
