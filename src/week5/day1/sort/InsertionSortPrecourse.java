502447820b01dfb57dfbaac51b676fe50686729f
chordpli <chordplaylist@gmail.com>
2022-10-17 00:39:42 +0900

    221017 InsertionSortPrecourse

--- src/week5/day1/sort/InsertionSortPrecourse.java
+++ src/week5/day1/sort/InsertionSortPrecourse.java
@@ -17,7 +17,19 @@ public class InsertionSortPrecourse {
             }
             i = idx;
         }
+        return arr;
+    }
 
+    public int[] sort2(int[] arr) {
+        for (int index = 1; index < arr.length; index++) {
+            int temp = arr[index];
+            int prev = index-1;
+            while ((prev >= 0) && (arr[prev] > temp)) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = temp;
+        }
         return arr;
     }
 
