63d92a35480a626329872de4b8c9c93f6123bf8e
chordpli <chordplaylist@gmail.com>
2022-10-16 17:52:00 +0900

    221016 InsertionSortPrecourse

---
+++ src/week5/day1/sort/InsertionSortPrecourse.java
@@ -0,0 +1,38 @@
+package week5.day1.sort;
+
+import java.util.Arrays;
+
+public class InsertionSortPrecourse {
+    public int[] sort(int[] arr) {
+        int idx;
+        for (int i = 1; i < arr.length; i++) {
+            idx = i;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] < arr[j]) {
+                    swap(arr, i, j);
+                    i--;
+                }else{
+                    break;
+                }
+            }
+            i = idx;
+        }
+
+        return arr;
+    }
+
+    public int[] swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 5, 6, 2, 4};
+        InsertionSortPrecourse isp = new InsertionSortPrecourse();
+
+        arr = isp.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
