a901e974fd9f27a453fbc600980ba68c4a5fd6d1
chordpli <chordplaylist@gmail.com>
2022-10-17 10:34:47 +0900

    221017 Rhombus, pyramid

---
+++ src/week5/day1/algorithm/repeatwrite/PyramidVer2.java
@@ -0,0 +1,27 @@
+package week5.day1.algorithm.repeatwrite;
+
+import java.util.Scanner;
+
+public class PyramidVer2 {
+    public void drawPyramid2(){
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 1; i <= count; i++) {
+            for (int j = 0; j < count-i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2*i-1; j++) {
+                System.out.print("*");
+            }
+            if (count != i) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PyramidVer2 pv = new PyramidVer2();
+        pv.drawPyramid2();
+    }
+}
---
+++ src/week5/day1/algorithm/repeatwrite/Rhombus2.java
@@ -0,0 +1,39 @@
+package week5.day1.algorithm.repeatwrite;
+
+import java.util.Scanner;
+
+public class Rhombus2 {
+
+    public void drawRhombus() {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i = 1; i <= count; i++) {
+            for (int j = 0; j < count - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = count-1; i >= 0; i--) {
+            for (int j = 0; j < count - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    public static void main(String[] args) {
+        Rhombus2 rb = new Rhombus2();
+        rb.drawRhombus();
+    }
+}
