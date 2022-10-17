1bbcd350d9a510a338d13bc07ae6f79bdeb394fe
chordpli <chordplaylist@gmail.com>
2022-10-17 09:31:10 +0900

    221017 RightTriangle, 도전과제

--- src/week5/day1/algorithm/repeatwrite/RightTriangle.java
+++ src/week5/day1/algorithm/repeatwrite/RightTriangle.java
@@ -1,19 +1,31 @@
-package week5.day1.algorithm.star;
+package week5.day1.algorithm.repeatwrite;
 
 import java.util.Scanner;
 
 public class RightTriangle {
+
+    private String word;
+
+    public RightTriangle(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
     public static void main(String[] args) {
         /*System.out.println("*");
         System.out.println("**");
         System.out.println("***");
         System.out.println("****");
         */
+        RightTriangle rt = new RightTriangle("#");
         Scanner sc = new Scanner(System.in);
         int count = sc.nextInt();
         for (int i = 1; i <= count; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(rt.getWord());
             }
             if (i != count) {
                 System.out.println();
