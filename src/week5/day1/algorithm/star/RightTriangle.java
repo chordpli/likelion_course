3e3fc69b31a6e75af1e8ca827f87823cf7f272f2
chordpli <chordplaylist@gmail.com>
2022-10-17 09:09:26 +0900

    221017 RightTriangle

---
+++ src/week5/day1/algorithm/star/RightTriangle.java
@@ -0,0 +1,18 @@
+package week5.day1.algorithm.star;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        /*System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");*/
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
