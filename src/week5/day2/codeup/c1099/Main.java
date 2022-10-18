c1bbadbd68d20c4a1ffda919de8c0c2446434285
chordpli <chordplaylist@gmail.com>
2022-10-19 08:14:00 +0900

    221018 codeup 리팩토링 필요

--- src/week5/day2/codeup/c1099/Main.java
+++ src/week5/day2/codeup/c1099/Main.java
@@ -1,28 +1,55 @@
 package week5.day2.codeup.c1099;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+        Main main = new Main();
         int[][] road = new int[10][10];
 
-        for (int i = 0; i <road.length ; i++) {
+        for (int i = 0; i < road.length; i++) {
             for (int j = 0; j < road[i].length; j++) {
                 road[i][j] = sc.nextInt();
             }
         }
-
-        Main main = new Main();
-        road = main.bfs(2, 2);
+        road = main.bfs(road, 1, 1);
+        for (int i = 0; i < road.length; i++) {
+            for (int j = 0; j < road[i].length; j++) {
+                System.out.print(road[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    public int[][] bfs(int a, int b) {
+    public int[][] bfs(int[][] road, int a, int b) {
+        if (road[a][b] == 2) {
+            road[a][b] = 9;
+            return road;
+        }else{
+            road[a][b] = 9;
+        }
 
-        return;
+        if (road[a][b + 1] == 0 || road[a][b + 1] == 2) {
+            if(road[a][b + 1] == 2){
+                road[a][b + 1] = 9;
+                return road;
+            }else{
+                road[a][b + 1] = 9;
+                return bfs(road, a, b + 1);
+            }
+        } else if (road[a + 1][b] == 0 || road[a + 1][b] == 2) {
+            if(road[a][b + 1] == 2){
+                road[a + 1][b] = 9;
+                return road;
+            }else{
+                return bfs(road, a + 1, b);
+            }
+        } else if (a == 8 && b == 8) {
+            road[a][b] = 9;
+            return road;
+        }
+        return road;
     }
 }
