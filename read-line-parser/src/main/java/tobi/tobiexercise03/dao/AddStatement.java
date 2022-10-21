98c4cbc79487b81ad1da3b0e036d408241aecd36
chordpli <chordplaylist@gmail.com>
2022-10-21 14:15:43 +0900

    221021 AddStatement

---
+++ read-line-parser/src/main/java/tobi/tobiexercise03/dao/AddStatement.java
@@ -0,0 +1,23 @@
+package tobi.tobiexercise03.dao;
+
+import tobi.tobiexercise03.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddStatement implements StatementStrategy{
+    User user;
+
+    public AddStatement(User user) {
+        this.user = user;
+    }
+    @Override
+    public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+        return null;
+    }
+}
