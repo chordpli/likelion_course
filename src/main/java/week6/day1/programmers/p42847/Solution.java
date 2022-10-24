7b0d84da38e92d3f027f5bbc1188e3843163795b
chordpli <chordplaylist@gmail.com>
2022-10-24 09:25:23 +0900

    221024 K번째 수 다시 풀어보기

--- src/main/java/week6/day1/programmers/p42847/Solution.java
+++ src/main/java/week6/day1/programmers/p42847/Solution.java
@@ -1,4 +1,12 @@
 package week6.day1.programmers.p42847;
 
+import java.util.Arrays;
+
 public class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        return answer;
+    }
+
 }
