f98f53ef6317ba76268635ca0f72bc966ae8ffb7
chordpli <chordplaylist@gmail.com>
2022-10-31 09:35:25 +0900

    221028 feat(모의고사): returnScore 메서드

---
+++ src/main/java/week7/day1/bruteforce/programmers/p42840/Solution.java
@@ -0,0 +1,23 @@
+package week7.day1.bruteforce.programmers.p42840;
+
+public class Solution {
+
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+
+        int[] firstGaveUp = {1, 2, 3, 4, 5};
+        int[] secondGaveUp = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] thirdGaveUp = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int firstGaveUpScore = returnScore(answer, firstGaveUp);
+
+
+        return answer;
+    }
+
+    public int returnScore(int[] answer, int[] firstGaveUp) {
+
+
+        return 0;
+    }
+}
