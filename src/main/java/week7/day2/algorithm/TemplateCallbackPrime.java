1bf3029b442804aadbab6df60c6b0382a806c2b5
chordpli <chordplaylist@gmail.com>
2022-11-01 10:50:14 +0900

    refactor: apply lambda

--- src/main/java/week7/day2/algorithm/TemplateCallbackPrime.java
+++ src/main/java/week7/day2/algorithm/TemplateCallbackPrime.java
@@ -17,7 +17,6 @@ public class TemplateCallbackPrime {
 
     public static void main(String[] args) {
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean r = tcp.isPrime(17);
-        System.out.println(r);
+        System.out.println(tcp.isPrime(17,(a,b) -> a*a <b));
     }
 }
