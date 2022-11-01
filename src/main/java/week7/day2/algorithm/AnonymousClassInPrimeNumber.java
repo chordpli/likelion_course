b3d76ceb77f9445c172dbd53d9e5d24575ccca9d
chordpli <chordplaylist@gmail.com>
2022-11-01 10:53:35 +0900

    feat: apply anonymousclass
    익명 함수 적용.
    이후 -> 람다 적용

---
+++ src/main/java/week7/day2/algorithm/AnonymousClassInPrimeNumber.java
@@ -0,0 +1,22 @@
+package week7.day2.algorithm;
+
+public class AnonymousClassInPrimeNumber {
+    boolean isPrime(int num, StatementStrategy stmt) {
+        for (int i = 2; stmt.compare(i, num); i++) {
+            System.out.println(i);
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        AnonymousClassInPrimeNumber acip = new AnonymousClassInPrimeNumber();
+        boolean r = acip.isPrime(17, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a <= b;
+            }
+        });
+        System.out.println(r);
+    }
+}
