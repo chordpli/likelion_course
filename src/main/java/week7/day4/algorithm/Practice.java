0799b548a0fc4f56e599817f771e67b19c1d643d
chordpli <chordplaylist@gmail.com>
2022-11-03 09:28:30 +0900

    feat: 에라토스테네스의 체 -ing

--- src/main/java/week7/day4/algorithm/Practice.java
+++ src/main/java/week7/day4/algorithm/Practice.java
@@ -2,14 +2,14 @@ package week7.day4.algorithm;
 
 public class Practice {
     public static void main(String[] args) {
-        int[] multiple = new int[101];
+        int multiple = 101;
         boolean[] checks = new boolean[101];
 
         checks[0] = checks[1] = true;
         int count = 0;
-        for (int i = 2; i * i <= multiple.length; i++) {
+        for (int i = 2; i * i <= multiple; i++) {
             if (!checks[i]) {
-                for (int j = i * i; j <= multiple.length; j += i) {
+                for (int j = i * i; j <= multiple; j += i) {
                     checks[j] = true;
                     count++;
                 }
