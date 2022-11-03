93b219d2b2a56fb26ed08f1b92aced06db8f4d28
chordpli <chordplaylist@gmail.com>
2022-11-04 04:19:31 +0900

    feat: Prime Number

--- src/main/java/week7/day4/algorithm/Practice02.java
+++ src/main/java/week7/day4/algorithm/Practice02.java
@@ -3,6 +3,19 @@ package week7.day4.algorithm;
 import java.util.Arrays;
 
 public class Practice02 {
+
+    public static void printNums(int[] nums, boolean[] checks) {
+        int cnt = 0;
+        for (int i = 0; i < checks.length; i++) {
+            if(checks[i]) {
+                System.out.printf("%d|", nums[i]);
+                cnt++;
+            }
+        }
+        System.out.println();
+        System.out.println("size:"+cnt);
+    }
+
     public static void main(String[] args) {
         int N = 50;
         int[] nums = new int [N - 1]; // 2~50까지 총 49개
@@ -19,6 +32,36 @@ public class Practice02 {
         for (int i = 2; i < nums.length; i += 2) {
             checks[i] = false;
         }
+        printNums(nums, checks);
 
+        multipleof = 3;
+        for (int i = 4; i < nums.length; i += 3) {
+            checks[i] = false;
+        }
+        printNums(nums, checks);
+
+        multipleof = 4;
+        for (int i = 6; i < nums.length; i += 4) {
+            checks[i] = false;
+        }
+        printNums(nums, checks);
+
+        multipleof = 5;
+        for (int i = 8; i < nums.length; i += 5) {
+            checks[i] = false;
+        }
+        printNums(nums, checks);
+
+        multipleof = 6;
+        for (int i = 10; i < nums.length; i += 6) {
+            checks[i] = false;
+        }
+        printNums(nums, checks);
+
+        multipleof = 7;
+        for (int i = 12; i < nums.length; i += 7) {
+            checks[i] = false;
+        }
+        printNums(nums, checks);
     }
 }
