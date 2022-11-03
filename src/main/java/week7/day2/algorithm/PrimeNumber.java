a00cdf9cc32df748d01a98793aa81eb6e530537f
chordpli <chordplaylist@gmail.com>
2022-11-04 04:19:02 +0900

    feat: Prime Number

--- src/main/java/week7/day2/algorithm/PrimeNumber.java
+++ src/main/java/week7/day2/algorithm/PrimeNumber.java
@@ -3,17 +3,31 @@ package week7.day2.algorithm;
 public class PrimeNumber {
 
     public boolean firstMethod(int number) {
-        int remainder = 1;
-
         for (int i = 2; i < number; i++) {
-            remainder = number % i;
-            if (remainder == 0) {
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
     }
 
+    public boolean secondMethod(int number) {
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean thirdMethod(int number) {
+        for (int i = 2; i*i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         PrimeNumber p = new PrimeNumber();
