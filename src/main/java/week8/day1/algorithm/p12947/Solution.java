f1baaa684163d814af55b2aa5c59513ac6ead72f
chordpli <chordplaylist@gmail.com>
2022-11-07 09:20:45 +0900

    feat: 하샤드의 수 완료

--- src/main/java/week8/day1/algorithm/p12947/Solution.java
+++ src/main/java/week8/day1/algorithm/p12947/Solution.java
@@ -2,7 +2,23 @@ package week8.day1.algorithm.p12947;
 
 public class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
-        return answer;
+        // 자릿수의 합 구하기
+        int sumDigits = findSumDigits(x);
+        // x로 나누어 떨어지는지 확인하기
+        return checkFixDivideNumber(x, sumDigits);
     }
+
+    public int findSumDigits(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+
+    public boolean checkFixDivideNumber(int x, int sumDigits) {
+        return x % sumDigits == 0;
+    }
+
 }
