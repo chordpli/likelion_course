0bbabef9e7cad696d848aa2b6f136caa1ed2418e
chordpli <chordplaylist@gmail.com>
2022-11-08 11:07:03 +0900

    feat: 이분탐색

--- src/main/java/week8/day2/algorithm/binarysearch/BinarySearch.java
+++ src/main/java/week8/day2/algorithm/binarysearch/BinarySearch.java
@@ -7,23 +7,25 @@ public class BinarySearch {
         Scanner sc = new Scanner(System.in);
         int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         int targetNumber = sc.nextInt();
+
         int startIdx = 0;
-        int lastIdx = nums.length;
+        int endIdx = nums.length - 1;
+
 
         int findIdx = -1;
         // 중간값 찾기
+        while (startIdx <= endIdx) {
+            int midIdx = (endIdx + startIdx) / 2;
+            int midValue = nums[midIdx];
 
-        for (int i = startIdx; i < lastIdx;) {
-            if (nums[lastIdx/2] == targetNumber) {
-                findIdx = lastIdx/2;
+            if (midValue > targetNumber) {
+                endIdx = midIdx - 1;
+            } else if (midValue < targetNumber) {
+                startIdx = midIdx + 1;
+            } else {
+                System.out.println(midIdx);
                 break;
-            } else if (nums[lastIdx / 2] > targetNumber) {
-                lastIdx /= 2;
-            } else{
-                startIdx = lastIdx / 2;
             }
         }
-
-        System.out.println(findIdx);
     }
 }
