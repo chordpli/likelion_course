08dfd19ae178310351e42bfc443db9866f244457
chordpli <chordplaylist@gmail.com>
2022-11-09 09:50:21 +0900

    feat: SelectionSort

--- src/main/java/week8/day3/algorithm/selectionsort/SelectionSort01.java
+++ src/main/java/week8/day3/algorithm/selectionsort/SelectionSort01.java
@@ -1,10 +1,35 @@
 package week8.day3.algorithm.selectionsort;
 
+import java.util.Arrays;
+
 public class SelectionSort01 {
 
     public static void main(String[] args) {
 
+        int[] arr = {2, 7, 4, 9, 10, 233, 1, 111, 23, 3, 39};
 
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                min = getMin(arr, min, j);
+            }
+            swap(arr, i, min);
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int[] arr, int i, int min) {
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+
+    private static int getMin(int[] arr, int min, int j) {
+        if (arr[min] > arr[j]) {
+            min = j;
+        }
+        return min;
     }
 
 }
