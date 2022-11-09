9c86ca3a53c6c2a22389e1ef06776916e7fdf850
chordpli <chordplaylist@gmail.com>
2022-11-09 10:49:39 +0900

    feat: SelectionSort strategy 적용

---
+++ src/main/java/week8/day3/algorithm/selectionsort/SelectionSort03.java
@@ -0,0 +1,35 @@
+package week8.day3.algorithm.selectionsort;
+
+import java.util.Arrays;
+interface SortStrategy{
+    boolean apply(int a, int b);
+}
+public class SelectionSort03 {
+
+    public int[] selectionSort(int[] arr, SortStrategy stmt) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i; j < arr.length; j++) {
+                if(stmt.apply(arr[minIdx], arr[j])) minIdx=j;
+            }
+            swap(arr, i, minIdx);
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 4, 9, 10, 233, 1, 111, 23, 3, 39};
+
+        SelectionSort03 s = new SelectionSort03();
+
+        s.selectionSort(arr, (a, b) -> a < b);
+        s.selectionSort(arr, (a, b) -> a > b);
+    }
+
+    public void swap(int[] arr, int i, int min) {
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
