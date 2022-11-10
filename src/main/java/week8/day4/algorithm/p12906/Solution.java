6289beef4efc53bc2a98843aca02078f1fb6f88e
chordpli <chordplaylist@gmail.com>
2022-11-10 09:26:19 +0900

    feat: p12906 같은 숫자는 싫어 구현

--- src/main/java/week8/day4/algorithm/p12906/Solution.java
+++ src/main/java/week8/day4/algorithm/p12906/Solution.java
@@ -1,14 +1,37 @@
 package week8.day4.algorithm.p12906;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        int[] answer = {};
+    public int[] solution(int[] arr) {
 
-        Stack<Integer> Number = new Stack<>();
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        Stack<Integer> number = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            inputStack(arr, number, i);
+        }
+        int[] answer = new int[number.size()];
+        for (int i = number.size()-1; i >= 0; i--) {
+            answer[i] = number.pop();
+        }
 
         return answer;
+    }
+
+    private void inputStack(int[] arr, Stack<Integer> number, int i) {
+        if (number.isEmpty()) {
+            number.add(arr[i]);
+        }else{
+            if (!(number.peek() == arr[i])) {
+                number.add(arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        System.out.println(Arrays.toString(s.solution(arr)));
     }
 }
