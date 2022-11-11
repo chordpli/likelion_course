d312058e7d4f7fa1c2d94356c6f0f0bf399ec282
chordpli <chordplaylist@gmail.com>
2022-11-11 09:08:35 +0900

    feat: p17681 비밀지도 주석

---
+++ src/main/java/week8/day5/algorithm/p17681/Solution.java
@@ -0,0 +1,24 @@
+package week8.day5.algorithm.p17681;
+
+public class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        // arr1을 2진수로 변환
+        // arr2를 2진수로 변환
+
+        // arr1과 arr2 OR 계산
+        // 5자리 미만인 경우 앞에 0을 추가.
+
+        // 1인 경우에는 #
+        // 0인 경우에는 " "을 출력한다.
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+    }
+}
