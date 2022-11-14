d4e583ea2693fd0316ac419688cd72a51d0950cb
chordpli <chordplaylist@gmail.com>
2022-11-14 10:50:41 +0900

    feat: 에라토스테네스의 체

--- src/main/java/week9/day1/algorithm/sieveoferatosthenes/Practice01.java
+++ src/main/java/week9/day1/algorithm/sieveoferatosthenes/Practice01.java
@@ -6,18 +6,18 @@ public class Practice01 {
 
     public static void main(String[] args) {
         int N = 50;
-        int[] arr = new int[N-1];
+        int[] arr = new int[N - 1];
 
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 2;
         }
 
-        for (int i = 2; i < arr.length ; i+=2) {
-            arr[i] = 0;
-        }
-
-        for (int i = 4; i < arr.length ; i+=3) {
-            arr[i] = 0;
+        for (int i = 0; i * i <= N; i++) {
+            if (arr[i] != 0) {
+                for (int j = i * 2 + 2; j < arr.length; j += arr[i]) {
+                    arr[j] = 0;
+                }
+            }
         }
 
         System.out.println(Arrays.toString(arr));
