afc875ee6c11bca1dafd95a059636eb8ae263f7c
chordpli <chordplaylist@gmail.com>
2022-11-16 09:32:47 +0900

    feat: arr을 이용한 quick sort

---
+++ src/main/java/week9/day3/algorithm/quicksort/Practice01.java
@@ -0,0 +1,46 @@
+package week9.day3.algorithm.quicksort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Practice01 {
+
+    public int[] quickSort(int[] arr) {
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 25, 5, 50, 40};
+
+        int pivot = arr[arr.length / 2];
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+        while (true) {
+            if (pivot > arr[leftIdx]) {
+                leftIdx += 1;
+            }
+
+            if (pivot < arr[rightIdx]) {
+                rightIdx -= 1;
+            }
+
+            if (arr[leftIdx] <= arr[rightIdx]) {
+                int temp = arr[leftIdx];
+                arr[leftIdx] = arr[rightIdx];
+                arr[rightIdx] = temp;
+
+                leftIdx += 1;
+                rightIdx -= 1;
+                break;
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
