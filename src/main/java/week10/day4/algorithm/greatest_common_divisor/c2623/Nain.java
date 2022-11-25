12787637990835c320d893b2e93d83b6a705a71d
chordpli <chordplaylist@gmail.com>
2022-11-25 09:43:35 +0900

    feat: 2623 : 최대공약수 구하기(재귀 안쓰고)

---
+++ src/main/java/week10/day4/algorithm/greatest_common_divisor/c2623/Nain.java
@@ -0,0 +1,22 @@
+package week10.day4.algorithm.greatest_common_divisor.c2623;
+
+import java.util.Scanner;
+
+public class Nain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        while (a != b) {
+            if (a > b) {
+                a -= b;
+            }
+            if (a < b) {
+                b -= a;
+            }
+        }
+        System.out.println(a);
+    }
+}
+
