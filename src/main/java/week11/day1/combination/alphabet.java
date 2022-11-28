fb399ab3ab9d3076cf36e93fabe95a76bf3cd877
chordpli <chordplaylist@gmail.com>
2022-11-28 09:09:50 +0900

    feat: combination alphabet

---
+++ src/main/java/week11/day1/combination/alphabet.java
@@ -0,0 +1,11 @@
+package week11.day1.combination;
+
+public class alphabet {
+    public static void main(String[] args) {
+        for (int i = 65; i <=90 ; i++) {
+            for (int j = 65; j <=90 ; j++) {
+                System.out.println((char) i+""+(char) j);
+            }
+        }
+    }
+}
