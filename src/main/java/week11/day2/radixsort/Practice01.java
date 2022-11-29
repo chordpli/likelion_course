0212959c53267dd83a271aecbdbd5064fc43848a
chordpli <chordplaylist@gmail.com>
2022-11-29 10:12:11 +0900

    feat: method화

--- src/main/java/week11/day2/radixsort/Practice01.java
+++ src/main/java/week11/day2/radixsort/Practice01.java
@@ -4,15 +4,19 @@ import java.util.Arrays;
 
 public class Practice01 {
     public static void main(String[] args) {
-        int[] arr = new int[10];
-
         int[] inputArr = {7, 4, 5, 9, 1, 0};
+        int[] arr = sort(inputArr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static int[] sort(int[] inputArr) {
+        int[] arr = new int[Arrays.stream(inputArr).max().getAsInt()+1];
         Arrays.fill(arr, -1);
 
         for (int i : inputArr) {
             arr[i] = i;
         }
-
-        System.out.println(Arrays.toString(arr));
+        return arr;
     }
 }
