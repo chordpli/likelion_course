f92af1a3c0a46cfaad78e2a5ef2eab956c7c6c19
chordpli <chordplaylist@gmail.com>
2022-12-07 10:42:52 +0900

    feat: 프로그래머스 더 맵게

--- src/main/java/week12/day3/algorithm/p42626/Solution.java
+++ src/main/java/week12/day3/algorithm/p42626/Solution.java
@@ -1,29 +1,34 @@
 package week12.day3.algorithm.p42626;
 
 import java.util.PriorityQueue;
+/*
+
+ 1. Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶다.
+ 2. 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수
+    + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+ 3. Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복
+ 4. Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
+    모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는
+    최소 횟수를 return 하도록 solution 함수를 작성해주세요.
+
+ */
 
 public class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int answer = 0;
 
-        for (int i = 0; i < scoville.length; i++) {
-            pq.offer(scoville[i]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int j : scoville) {
+            pq.add(j);
         }
 
-        while (!pq.isEmpty()) {
-            int curr = pq.poll();
-
-            if (curr < K) {
-                if (pq.size() == 0) {
-                    return -1;
-                }
-                int next = pq.poll();
-                int sum = curr + next * 2;
-                pq.offer(sum);
-                answer++;
-            }
+        while (pq.size() >= 2 && pq.peek() < K) {
+            int mix = pq.poll() + (pq.poll() * 2);
+            pq.add(mix);
+            answer++;
         }
+        if (pq.peek() < K) return -1;
         return answer;
     }
 }
