fd9e8b18797adf3b3ec97d479df3d9439f74e5fe
chordpli <chordplaylist@gmail.com>
2022-12-09 09:24:58 +0900

    feat: dp loop 구성

---
+++ src/main/java/week12/day5/algorithm/dp/LCS.java
@@ -0,0 +1,17 @@
+package week12.day5.algorithm.dp;
+
+public class LCS {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA";
+        String str2 = "DCABDC";
+
+        int[][] dp = new int [str2.length()][str1.length()];
+
+        for (int i = 0; i <str2.length() ; i++) {
+            for (int j = 0; j <str1.length() ; j++) {
+                System.out.printf("i:%s j:%s\n", str2.charAt(i), str1.charAt(j));
+            }
+
+        }
+    }
+}
